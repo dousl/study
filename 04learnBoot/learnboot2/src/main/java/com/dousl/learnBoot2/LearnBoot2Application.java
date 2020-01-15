9dbdcd5575953adfe4caca0ecd39cd9b56f02a9f
dousl <384602183@qq.com>
2020-01-15 14:25:30 +0800

    学习数据库连接

---
+++ 04learnBoot/learnboot2/src/main/java/com/dousl/learnBoot2/LearnBoot2Application.java
@@ -0,0 +1,13 @@
+package com.dousl.learnBoot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBoot2Application.class, args);
+	}
+
+}
---
+++ 04learnBoot/learnboot2/src/test/java/com/dousl/learnBoot2/LearnBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dousl.learnBoot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
