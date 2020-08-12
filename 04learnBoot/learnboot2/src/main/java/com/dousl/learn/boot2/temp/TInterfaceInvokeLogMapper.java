cb4078f97efd52eaae550a581bce704210287b82
dousl <384602183@qq.com>
2020-08-12 17:09:56 +0800

    删除无用代码

--- 04learnBoot/learnboot2/src/main/java/com/dousl/learn/boot2/temp/TInterfaceInvokeLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.dousl.learn.boot2.temp;
-
-import com.dousl.learn.boot2.temp.TInterfaceInvokeLog;
-
-public interface TInterfaceInvokeLogMapper {
-    int insert(TInterfaceInvokeLog record);
-
-    int insertSelective(TInterfaceInvokeLog record);
-}
